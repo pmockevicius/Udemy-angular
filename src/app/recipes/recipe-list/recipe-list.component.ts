1d05d9ba689386639faf3ec1020f53d4e888e1a4
Paulius Mockevicius <pmockevicius@gmail.com>
2023-05-18 11:31:12 +0200

    configure recipe list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+recipes: Recipe[] = [
+  new Recipe('A Test Recipe', 'This is a test', "https://images.immediate.co.uk/production/volatile/sites/30/2023/04/Trifle-bowl-coronation-salad-0ad63bf.jpg?quality=90&resize=556,505" ),
+  new Recipe('A Test Recipe', 'This is a test', "https://images.immediate.co.uk/production/volatile/sites/30/2023/04/Trifle-bowl-coronation-salad-0ad63bf.jpg?quality=90&resize=556,505" ),
+]
 }
